2eed6e11ab5266a40213467bc928cf02024b94ce
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-04 00:48:12 +0100

    Comments

--- store/todoSlice.js
+++ store/todoSlice.js
@@ -1,14 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+/**
+ * Starter Guide for redux toolkit : https://redux-toolkit.js.org/introduction/getting-started
+ * Good place to find the Documentation for manipulating arrays :https://redux-toolkit.js.org/usage/immer-reducers
+ */
 const todoSlice = createSlice({
   name: "todo",
   initialState: [],
   reducers: {
+    //Create like useState function where action is the value we send and state is our state
     setTasks(state, action) {
       state.push(action.payload);
     },
     setChecked(state, action) {
-      //TODO:Make checked of individual id
       const todo = state.find((todo) => todo.id === action.payload);
       if (todo) {
         // ✅ CORRECT: This object is still wrapped in a Proxy, so we can "mutate" it
