e40a94394b7894172ab0001675c0b505faf7081a
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-03 17:32:09 +0100

    isntall rneui

--- App.js
+++ App.js
@@ -1,55 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import { Text, View } from "react-native";
+import { Tab } from "@rneui/themed";
 
-import React, { Component } from "react";
-import { StyleSheet, View, Button, Text } from "react-native";
-import { connect } from "react-redux";
-import { changeCount } from "./actions/counts";
-import { bindActionCreators } from "redux";
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
 
-class App extends Component {
-  decrementCount() {
-    let { count, actions } = this.props;
-    count--;
-    actions.changeCount(count);
-  }
-  incrementCount() {
-    let { count, actions } = this.props;
-    count++;
-    actions.changeCount(count);
-  }
-  render() {
-    const { count } = this.props;
-    return (
-      <View styles={styles.container}>
-        <Button title="increment" onPress={() => this.incrementCount()} />
-        <Text>{count}</Text>
-        <Button title="decrement" onPress={() => this.decrementCount()} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
-
-const mapStateToProps = (state) => ({
-  count: state.count,
-});
-
-const ActionCreators = Object.assign({}, changeCount);
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(ActionCreators, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
