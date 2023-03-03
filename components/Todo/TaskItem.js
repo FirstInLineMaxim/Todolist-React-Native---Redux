f7399d6390178b27142b8c72715ff1c73f073ec9
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-03-03 19:55:43 +0100

    TaskItem

--- components/Todo/TaskItem.js
+++ components/Todo/TaskItem.js
@@ -1,5 +1,29 @@
+import { ListItem } from "@rneui/base";
+import { Text } from "@rneui/themed";
 import React from "react";
+import { StyleSheet, View } from "react-native";
 
-export default function TaskItem() {
-  return <div>TaskItem</div>;
+const styles = StyleSheet.create({
+  done: {
+    textDecorationLine: "line-through",
+    color: "grey",
+  },
+});
+export default function TaskItem({ index, task }) {
+  return (
+    <View>
+      <ListItem value="test" bottomDivider onPress={(e) => console.log(e)}>
+        <ListItem.CheckBox
+          // Use ThemeProvider to change the defaults of the checkbox
+          iconType="material-community"
+          checkedIcon="checkbox-marked"
+          uncheckedIcon="checkbox-blank-outline"
+          // checked={checked[0]}
+        />
+        <ListItem.Content>
+          <ListItem.Title style={styles.done}>{task}</ListItem.Title>
+        </ListItem.Content>
+      </ListItem>
+    </View>
+  );
 }
--- components/Todo/Todo.js
+++ components/Todo/Todo.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import { Text } from "react-native";
 
 import { ListItem } from "@rneui/themed";
+import TaskItem from "./TaskItem";
 export default function Todo() {
   const [checked, setChecked] = useState({ task: "", checked: "" });
-  return <></>;
+  return (
+    <>
+      <TaskItem index="1" task="test" />
+    </>
+  );
 }
